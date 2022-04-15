55fc26bfd33796153a537373683e9b538c85ce5c
francis omondi <francisomondi17@gmail.com>
2022-04-16 00:40:33 +0300

    installation of font awesone and added a delete icon

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
 
+import { FaTimes } from "react-icons/fa";
 const Task = ({ task }) => {
     return (
         <div className='task'>
 
-            <h3>{task.description}</h3>
+            <h3>{task.description}<FaTimes /></h3>
             <p>{task.day}</p>
 
         </div>
