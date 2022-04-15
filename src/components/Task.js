4da57d8500a16f06a1fa729e93f8f6287e873736
francis omondi <francisomondi17@gmail.com>
2022-04-16 01:02:57 +0300

    red delete  button

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,7 @@ const Task = ({ task }) => {
     return (
         <div className='task'>
 
-            <h3>{task.description}<FaTimes /></h3>
+            <h3>{task.description}<FaTimes style={{ color: 'red', cursor: 'pointer' }} /></h3>
             <p>{task.day}</p>
 
         </div>
